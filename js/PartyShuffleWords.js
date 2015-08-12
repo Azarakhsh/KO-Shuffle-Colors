c62a60aeb5fe105f28ce8a88ab546102a118268d
azarakhsh <azarakhsh.sh@gmail.com>
2015-08-13 04:06:21 +0430

    Calculates-spectrum

--- js/PartyShuffleWords.js
+++ js/PartyShuffleWords.js
@@ -5,14 +5,41 @@
         this.inputText = ko.observable("this is my awesome text");
         this.initialRGB = ko.observable("100,100,100");
         this.initialFactor = ko.observable(10);
+        
+        this.spectrum = ko.computed(function(){
+            words = this.inputText().split(' ');
+            rgbBase = this.initialRGB().split(',');
 
-        this.createSpectrums = function() {
-            self = this;
-            self.words = self.inputText().split(' ');
-            self.rgbBase = self.initialRGB().split(',');
+            var plusminus,
+                factor,
+                result = [];
             
-                
-        };
+  
+            for (var i = 0; i < words.length; i++) {
+                result[i] = [];
+                factor = this.initialFactor();
+                x = Math.random()
+                if (0.5>x){ 
+                    factor = -this.initialFactor();
+                }
+                plusminus = (Math.floor( Math.random() * words.length )+1)*factor;
+
+
+                for (var j = 0; j < 3; j++) {
+                    if ( (+rgbBase[j] + plusminus)>= 255 || (+rgbBase[j] + plusminus)<= 0 ) {
+                    
+                        j=Math.min(j-1,0);//?
+                        factor = -factor;
+                        plusminus = (Math.floor( Math.random() * words.length )+1)*factor;
+                    }else{
+                        result[i][j] = (+rgbBase[j] + plusminus);
+                    }
+
+                };
+            };
+            return result;
+            console.log(result);  
+        }, this);
     }
 
     // Activates knockout.js
